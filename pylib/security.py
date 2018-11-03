33d2d0865d3421f2d7f8873befe11dfa2855843f
Larry Xu <hilarryxu@gmail.com>
2018-11-03 11:01:23 +0800

    Update _compat.py

--- pylib/security.py
+++ pylib/security.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import hmac
 from random import SystemRandom
 
-from ._compat import range_type
-
+from ._compat import range_type, text_type
 
 SALT_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-
 
 _sys_rng = SystemRandom()
 
@@ -16,3 +15,11 @@ def gen_salt(length):
     if length <= 0:
         raise ValueError('Salt length must be positive')
     return ''.join(_sys_rng.choice(SALT_CHARS) for _ in range_type(length))
+
+
+def password_hash(password, salt):
+    if isinstance(password, text_type):
+        password = password.encode('utf-8')
+    if isinstance(salt, text_type):
+        salt = salt.encode('utf-8')
+    return hmac.new(salt, password).hexdigest()
